6ebc15f6b7b4ee566d78baff5e61568b94ca3856
Killian0812 <ngcuong0812@gmail.com>
2025-08-15 13:45:29 +0700

    doing: integration test

--- order-service/src/test/java/com/killian/microservices/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/killian/microservices/order_service/OrderServiceApplicationTests.java
@@ -1,15 +1,39 @@
 package com.killian.microservices.order_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+
+import io.restassured.RestAssured;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
+	@Autowired
+	private MySQLContainer<?> mysqlContainer;
+
+	@LocalServerPort
+	private Integer port;
+
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateOrder() {
+		String requestBody = """
+				{
+		"skuCode": "iphone_15",
+		"price": 10000,
+		"quantity": 1
+		}
+				""";
 	}
 
 }
