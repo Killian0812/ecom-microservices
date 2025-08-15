a20de4c2c75b36d3118d3b8bc3c5948dc8aa25f7
Killian0812 <ngcuong0812@gmail.com>
2025-08-15 16:37:35 +0700

    fix: test - api & chain db query

--- order-service/src/test/java/com/killian/microservices/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/killian/microservices/order_service/OrderServiceApplicationTests.java
@@ -1,39 +1,61 @@
 package com.killian.microservices.order_service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.containers.MySQLContainer;
+
+import com.killian.microservices.order_service.repository.OrderRepository;
 
 import io.restassured.RestAssured;
 
+@TestMethodOrder(OrderAnnotation.class)
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
 	@Autowired
-	private MySQLContainer<?> mysqlContainer;
+	private OrderRepository orderRepository;
 
 	@LocalServerPort
 	private Integer port;
 
+	@BeforeEach
 	void setup() {
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.port = port;
 	}
 
 	@Test
+	@Order(1)
 	void shouldCreateOrder() {
 		String requestBody = """
-				{
-		"skuCode": "iphone_15",
-		"price": 10000,
-		"quantity": 1
-		}
-				""";
+						{
+				"skuCode": "iphone_15",
+				"price": 10000,
+				"quantity": 1
+				}
+						""";
+
+		RestAssured.given()
+				.contentType("application/json").body(requestBody).when().post("/api/order").then()
+				.statusCode(200);
 	}
 
+	@Test
+	@Order(2)
+	void shouldQueryPlacedOrderFromDatabase() {
+		var orderOpt = orderRepository.findFirstBySkuCode("iphone_15");
+		assertThat(orderOpt).isPresent();
+		assertThat(orderOpt.get().getQuantity()).isEqualTo(1);
+		assertThat(orderOpt.get().getPrice())
+				.isEqualByComparingTo("10000");
+	}
 }
--- order-service/src/test/java/com/killian/microservices/order_service/TestcontainersConfiguration.java
+++ order-service/src/test/java/com/killian/microservices/order_service/TestcontainersConfiguration.java
@@ -12,7 +12,7 @@ class TestcontainersConfiguration {
 	@Bean
 	@ServiceConnection
 	MySQLContainer<?> mysqlContainer() {
-		return new MySQLContainer<>(DockerImageName.parse("mysql:8.3"));
+		return new MySQLContainer<>(DockerImageName.parse("mysql:8.3.0"));
 	}
 
 }
