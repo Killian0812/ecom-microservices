5377973466ceab1530cbd3692494138269e91201
Killian0812 <ngcuong0812@gmail.com>
2025-08-16 15:21:55 +0700

    add inventory test

--- inventory-service/src/test/java/com/killian/microservices/inventory_service/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/killian/microservices/inventory_service/InventoryServiceApplicationTests.java
@@ -1,15 +1,48 @@
 package com.killian.microservices.inventory_service;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
+import io.restassured.RestAssured;
+
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InventoryServiceApplicationTests {
 
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldReadInventory() {
+		var response = RestAssured.given()
+				.when()
+				.get("/api/inventory?skuCode=IPHONE-15&quantity=1")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract().response().as(Boolean.class);
+		assertTrue(response);
+
+		var negativeResponse = RestAssured.given()
+				.when()
+				.get("/api/inventory?skuCode=IPHONE-15&quantity=1000")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract().response().as(Boolean.class);
+		assertFalse(negativeResponse);
+
 	}
 
 }
--- inventory-service/src/test/java/com/killian/microservices/inventory_service/TestcontainersConfiguration.java
+++ inventory-service/src/test/java/com/killian/microservices/inventory_service/TestcontainersConfiguration.java
@@ -11,8 +11,8 @@ class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	public PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16"));
 	}
 
 }
