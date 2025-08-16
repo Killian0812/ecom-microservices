60d53f8b5e06315fcb9bdd87f2d5c00e73d1b529
Killian0812 <ngcuong0812@gmail.com>
2025-08-16 14:35:26 +0700

    init postgresql

---
+++ inventory-service/README.md
@@ -0,0 +1,3 @@
+No need init.sql for container startup \
+auto create based on POSTGRES_DB: inventory_service \
+![alt text](readme-assets/image.png)
---
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+    inventory (sku_code, quantity)
+VALUES
+    ('IPHONE-15', 100),
+    ('SAMSUNG-S24', 75),
+    ('PS5', 50),
+    ('NINTENDO-SWITCH', 120),
+    ('MACBOOK-AIR', 30);
