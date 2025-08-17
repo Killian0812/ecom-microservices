4a1a7690618160c107cad79e5e4d519f54c40116
Cuong Nguyen <ngcuong0812@gmail.com>
2025-08-17 23:50:33 +0700

    inventory feign client

--- order-service/src/main/java/com/killian/microservices/order_service/service/OrderService.java
+++ order-service/src/main/java/com/killian/microservices/order_service/service/OrderService.java
@@ -3,6 +3,7 @@ package com.killian.microservices.order_service.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.killian.microservices.order_service.client.InventoryClient;
 import com.killian.microservices.order_service.dto.OrderRequest;
 import com.killian.microservices.order_service.model.Order;
 import com.killian.microservices.order_service.repository.OrderRepository;
@@ -13,7 +14,14 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private InventoryClient inventoryClient;
+
     public void placeOrder(OrderRequest request) {
+        boolean isProductInStock = inventoryClient.isInStock(request.skuCode(), request.quantity());
+        if (!isProductInStock) {
+            throw new RuntimeException("Product is not in stock");
+        }
         Order order = Order.fromRequest(request);
         orderRepository.save(order);
     }
