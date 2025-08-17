2184b6d501613410137261df0e9ecba525330645
Cuong Nguyen <ngcuong0812@gmail.com>
2025-08-18 00:24:34 +0700

    inventory service stub & refactor test

--- order-service/src/test/java/com/killian/microservices/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/killian/microservices/order_service/OrderServiceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.killian.microservices.order_service;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
@@ -10,15 +12,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 
 import com.killian.microservices.order_service.repository.OrderRepository;
+import com.killian.microservices.order_service.stub.InventoryServiceStub;
 
 import io.restassured.RestAssured;
 
 @TestMethodOrder(OrderAnnotation.class)
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@Autowired
@@ -27,35 +32,39 @@ class OrderServiceApplicationTests {
 	@LocalServerPort
 	private Integer port;
 
+	private static final String SKU_CODE = "IPHONE-15";
+	private static final int PRICE = 10000;
+	private static final int QUANTITY = 1;
+	private static final String ORDER_ENDPOINT = "/api/order";
+
 	@BeforeEach
+	@SuppressWarnings("unused")
 	void setup() {
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.port = port;
+
+		InventoryServiceStub.stubInventoryCall(SKU_CODE, QUANTITY);
 	}
 
 	@Test
 	@Order(1)
 	void shouldCreateOrder() {
-		String requestBody = """
-						{
-				"skuCode": "iphone_15",
-				"price": 10000,
-				"quantity": 1
-				}
-						""";
-
+		Map<String, Object> requestBody = Map.of(
+				"skuCode", SKU_CODE,
+				"price", PRICE,
+				"quantity", QUANTITY);
 		RestAssured.given()
-				.contentType("application/json").body(requestBody).when().post("/api/order").then()
+				.contentType("application/json").body(requestBody).when().post(ORDER_ENDPOINT).then()
 				.statusCode(200);
 	}
 
 	@Test
 	@Order(2)
 	void shouldQueryPlacedOrderFromDatabase() {
-		var orderOpt = orderRepository.findFirstBySkuCode("iphone_15");
+		var orderOpt = orderRepository.findFirstBySkuCode(SKU_CODE);
 		assertThat(orderOpt).isPresent();
-		assertThat(orderOpt.get().getQuantity()).isEqualTo(1);
+		assertThat(orderOpt.get().getQuantity()).isEqualTo(QUANTITY);
 		assertThat(orderOpt.get().getPrice())
-				.isEqualByComparingTo("10000");
+				.isEqualByComparingTo(Integer.toString(PRICE));
 	}
 }
