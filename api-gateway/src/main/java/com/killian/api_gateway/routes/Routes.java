b6572b6085ca2fa911e6e5d03645fbe57c8ba035
Killian0812 <ngcuong0812@gmail.com>
2025-08-18 14:32:44 +0700

    config routes

---
+++ api-gateway/src/main/java/com/killian/api_gateway/routes/Routes.java
@@ -0,0 +1,42 @@
+package com.killian.api_gateway.routes;
+
+import org.springframework.beans.factory.annotation.Value;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration(proxyBeanMethods = false)
+public class Routes {
+    @Value("${product.service.url}")
+    private String productServiceUrl;
+    @Value("${order.service.url}")
+    private String orderServiceUrl;
+    @Value("${inventory.service.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
+        return route("product_service")
+                .route(RequestPredicates.path("/api/product"), http(productServiceUrl))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return route("order_service")
+                .route(RequestPredicates.path("/api/order"), http(orderServiceUrl))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return route("inventory_service")
+                .route(RequestPredicates.path("/api/inventory"), http(inventoryServiceUrl))
+                .build();
+    }
+
+}
