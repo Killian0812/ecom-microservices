53f5bd4c256b46611f5ea2bef67adb9e95c6b71e
Killian0812 <ngcuong0812@gmail.com>
2025-08-26 11:30:52 +0700

    implement circuit breaker for order service

--- order-service/src/main/java/com/killian/microservices/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/killian/microservices/order_service/client/InventoryClient.java
@@ -1,9 +1,25 @@
 package com.killian.microservices.order_service.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+
 public interface InventoryClient {
+
+    static Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    // Executed whenever the circuit breaker state is OPEN
+    default boolean fallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
+        log.error("Fallback method called due to: {}", throwable.getMessage());
+        return false;
+    }
 }
