8c3a8a5badc45f073d03b57f4cb2b572866e7d9a
Killian0812 <ngcuong0812@gmail.com>
2025-08-28 15:32:15 +0700

    config kafka & send message

--- order-service/src/main/java/com/killian/microservices/order_service/dto/OrderRequest.java
+++ order-service/src/main/java/com/killian/microservices/order_service/dto/OrderRequest.java
@@ -2,6 +2,9 @@ package com.killian.microservices.order_service.dto;
 
 import java.math.BigDecimal;
 
-public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity) {
+public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity,
+        UserDetails userDetails) {
 
+    public record UserDetails(String email, String name) {
+    }
 }
--- order-service/src/main/java/com/killian/microservices/order_service/service/OrderService.java
+++ order-service/src/main/java/com/killian/microservices/order_service/service/OrderService.java
@@ -1,10 +1,12 @@
 package com.killian.microservices.order_service.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.killian.microservices.order_service.client.InventoryClient;
 import com.killian.microservices.order_service.dto.OrderRequest;
+import com.killian.microservices.order_service.event.OrderPlacedEvent;
 import com.killian.microservices.order_service.model.Order;
 import com.killian.microservices.order_service.repository.OrderRepository;
 
@@ -17,6 +19,9 @@ public class OrderService {
     @Autowired
     private InventoryClient inventoryClient;
 
+    @Autowired
+    private KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+
     public void placeOrder(OrderRequest request) {
         boolean isProductInStock = inventoryClient.isInStock(request.skuCode(), request.quantity());
         if (!isProductInStock) {
@@ -24,5 +29,8 @@ public class OrderService {
         }
         Order order = Order.fromRequest(request);
         orderRepository.save(order);
+
+        OrderPlacedEvent event = new OrderPlacedEvent(order.getOrderNumber(), request.userDetails().email());
+        kafkaTemplate.send("order-placed", event);
     }
 }
